46d245abe1d11450d9d55aadb2fd07b3cf40f927
Ben Smith <benSmith1981@users.noreply.github.com>
2022-06-09 10:05:31 +0100

    REgister notifications for MVVM

---
+++ CoBCCanteen/CoBCCanteen/CoBCCanteen/Views/MessageKeys.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoBCCanteen.Views
+{
+    public class MessageKeys
+    {
+        public const string emailTaken = "EmailTaken";  
+        public const string idTaken = "IDTaken";
+    }
+}
